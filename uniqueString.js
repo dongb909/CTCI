0bac182476b9788bca3b723cbea1cccbd4f1a7b2
bacai dong <dongb909@gmail.com>
2020-01-13 13:02:49 -0800

    Create algorithm for isUnique1

--- uniqueString.js
+++ uniqueString.js
@@ -1,10 +1,48 @@
 // Question: Does string have all unique characters?
+//ex: string1 = 'jlkfsd' ==> true
+//ex: string2 = 'aabbcc' ==> false
 
 // Solution 1, using additional data structure aka object
+//Declare function
+var isUnique1 = string => {
+  //Declare empty object
+  var char = {};
+//Iterate through string
+  for (var i = 0; i < string.length; i ++) {
+    //check to see if character already in obj
+    //if yes then return false
+    console.log(char)
+    if (char.hasOwnProperty(string[i]) === true) {
+      return false
+    } else {
+      //else add character to obj and set to 1
+      char[string[i]] = 1
+      console.log(char)
+    }
+  }
+  //if go through end of string then return true
+  return true
+}
+console.log(isUnique1('abccdefg'))
 
-
-
+//Big O (N)
 
 //========================================================================
 // Solution 2, no additional structures used
 
+//Declare function
+var isUnique2 = string => {
+//Iterate through string (i)
+  for (var i = 0; i < string.length; i++) {
+    for (var j = 1; j < string.length; j ++) {
+
+    
+  
+  //iterate through remaining characters (j) to see if match
+
+  //if match then break and return false
+    }
+  }
+
+  //if complete then return true
+}
