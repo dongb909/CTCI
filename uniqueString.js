54dcc583a6237727c5c142abaa2bd05102e3ccce
bacai dong <dongb909@gmail.com>
2020-01-13 10:30:49 -0800

    First commit for CTCI practice

---
+++ uniqueString.js
@@ -0,0 +1,10 @@
+// Question: Does string have all unique characters?
+
+// Solution 1, using additional data structure aka object
+
+
+
+
+//========================================================================
+// Solution 2, no additional structures used
+
