1e68186405041ec9def2b2a3b58a14be554b6340
bacai dong <dongb909@gmail.com>
2020-01-13 18:35:58 -0800

    Complete one edit away

--- strings/oneAway.js
+++ strings/oneAway.js
@@ -9,3 +9,55 @@
       pale, bake => false
 */
 
+//get length of each str
+//check if length difference > 1 then return false
+//if length equal, 
+//check if both equal aka 0 edit
+//iterate through each character and count number of different index
+//if count > 1 then false
+
+//if not equal length then check if indexes equal (skipping over 1 index if not equal)
+//count number of skips
+//if > 1 then false
+var oneEdit = (str1, str2) => {
+  var len1 = str1.length;
+  var len2 = str2.length;
+  //IF EQUAL LENGTH
+  if (len1 === len2) {
+    var charDiff = 0;
+    var i = 0
+    while (i < len1) {
+      if (charDiff > 1) {
+        return false;
+      } else if (str1[i]!==str2[i]) {
+        charDiff++;
+      } 
+      i++
+    }
+    return true;
+  }
+  
+  //IF LENGTH DIFFERENCE IS > 1
+  if (Math.abs(len1 - len2) > 1) {
+    return false;
+  }
+  //ELSE CHECK IF ALL CHAR OF SHORTEST IS IN LONGEST
+  var big = str1;
+  var small = str2;
+  if (len2 > len1) {
+    big = str2;
+    small = str1;
+  } 
+  var i = 0
+  while (i < small.length) {
+    if (big.indexOf(small[i]) === -1) {
+      return false;
+    } else {
+      i++;
+    }
+  }
+  return true;
+}
+
+// console.log(oneEdit('pale','uile')) //check equal length
+console.log(oneEdit('bake', 'bale')) //check unequal length
