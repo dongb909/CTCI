792433e01725608609dbc65632685d6c1ce1c35f
bacai dong <dongb909@gmail.com>
2020-01-13 19:15:07 -0800

    Create isSubstring function

--- strings/rotateMatrix.js
+++
@@ -1,4 +0,0 @@
-/*Given an image represented by an NxN matrix, where each pixel in the image is represented by an integer,
-write a method to rotate the image by 90 degrees.
-Can you do this in place?
-*/
--- strings/stringRotation.js
+++ strings/stringRotation.js
@@ -1,2 +1,20 @@
-/*Assume you have a method isSubstring which checks if one word is a substring of another. Given 2 strings s1 and s2, write code to check if s2 is a rotation of s1 using only 1 call to isSubstring
-ex: 'waterbottle' is rotation of 'erbottlewat' */
+/*Assume you have a method isSubstring which checks if one word is a substring of another. 
+Given 2 strings s1 and s2, write code to check if s2 is a rotation of s1 using only 1 call to isSubstring
+ex: 'waterbottle' is rotation of 'aterbottlew' */
+//NOTE HOW THEY SPECIFIED WHICH STRING IS ROTATION OF WHICH!
+//ALSO THEY ALREADY NAMED THE FUNCTION FOR YOU SO DO NOT RENAME
+
+//duplicate str1 and combine into one. Then check if the str2 is a substring of str1 with method "includes"
+
+var isSubstring = (s1, s2) => {
+  //if strings aren't equal or if empty then false 
+  if (s1.length === 0 || s2.length === 0 || s1.length !== s2.length) {
+    return false;
+  } 
+  s1 = s1 + s1;
+  if (s1.includes(s2)) {
+    return true;
+  }
+  return false;
+}
+console.log (isSubstring('waterbottle', 'aterbottlew'))
